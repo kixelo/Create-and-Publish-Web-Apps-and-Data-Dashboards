d10ec36c0150ffd65960a94b52bd3266d2720a7a
Igor O <94803838+kixelo@users.noreply.github.com>
2022-12-10 18:01:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas
+
+st.title('Our first Streamlit App')
+st.subheader('Introduction to Streamlit and Python')
+st.write('''This is our first Web App
+Enjoy it!
+''')
+
