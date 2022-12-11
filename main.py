d8e3f4bfe513ca17f857d6459073f77ba6321c7b
Igor O <94803838+kixelo@users.noreply.github.com>
2022-12-11 08:43:19 +0000

    Table added

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import streamlit as st
 import pandas
 
+data = {
+  "Series_1":[1,3,4,5,7], 
+  "Series_2": [10,30,40,100,250]}
+
+df=pandas.DataFrame(data)
+#print(df)
+
 st.title('Our first Streamlit App')
 st.subheader('Introduction to Streamlit and Python')
 st.write('''This is our first Web App
 Enjoy it!
 ''')
-
+st.write(df)
